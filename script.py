46887d96c17cbd80643d1c6af4559b87fa67846d
devhindo <lildevhind@gmail.com>
2024-03-24 06:53:41 +0200

    added paste functionality

--- script.py
+++ script.py
@@ -1,18 +1,29 @@
 import speech_recognition as sr
 import pyperclip
+import pyautogui
 
 # create recognizer and mic instances
 recognizer = sr.Recognizer()
 microphone = sr.Microphone()
 
-# listen to the microphone
-with microphone as source:
-    print("Say something!")
-    audio = recognizer.listen(source)
+while True:
+    # listen to the microphone
+    with microphone as source:
+        print("Say something!")
+        audio = recognizer.listen(source)
 
-# recognize speech using Google Speech Recognition
-words = recognizer.recognize_google(audio)
+    try:
+        # recognize speech using Google Speech Recognition
+        words = recognizer.recognize_google(audio)
+        print(words)
 
-# perform command if 'paste' is heard
-if 'paste' in words:
-    pyperclip.paste()
+        # perform command if 'paste' is heard
+        if 'paste' in words:
+            pyperclip.paste()
+        elif 'stop listening' in words:
+            break
+        if 'paste' in words:
+            clipboard_content = pyperclip.paste()
+            pyautogui.write(clipboard_content)
+    except sr.UnknownValueError:
+        print("Sorry, I did not understand that.")
